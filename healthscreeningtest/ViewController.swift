125c77987dbb291dda3e8d91ec5c5dcf232c63d6
Derrick Duller <derrness@gmail.com>
2022-02-18 17:58:20 -0500

    Initial Commit

---
+++ healthscreeningtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthscreeningtest
+//
+//  Created by Derrick Duller on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
